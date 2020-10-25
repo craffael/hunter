32f84509c31ece4a9312c5fa4053c6582e228d46
craffael <raffael@casagrande.ch>
2020-10-25 13:00:55 +0100

    bugfix

--- cmake/projects/occt/ci/build.sh
+++ cmake/projects/occt/ci/build.sh
@@ -1,15 +1,18 @@
 # Install additional dependencies:
 
 if [ "$(uname)" == "Darwin" ]; then
-    # OSX     
+    # OSX    
+    :
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # GNU/Linux
     sudo apt-get update
     sudo apt-get install mesa-common-dev libgl1-mesa-dev
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under 32 bits Windows NT platform
+    :
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # Do something under 64 bits Windows NT platform
+    :
 fi
 
 

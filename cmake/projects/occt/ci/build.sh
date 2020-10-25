e206de41107373dab5759e80b6bd6d32f3a40697
craffael <raffael@casagrande.ch>
2020-10-25 12:58:44 +0100

    add another package...

--- cmake/projects/occt/ci/build.sh
+++ cmake/projects/occt/ci/build.sh
@@ -1,5 +1,17 @@
 # Install additional dependencies:
-sudo apt-get install mesa-common-dev
+
+if [ "$(uname)" == "Darwin" ]; then
+    # OSX     
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # GNU/Linux
+    sudo apt-get update
+    sudo apt-get install mesa-common-dev libgl1-mesa-dev
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+fi
+
 
 # Run normal build script: 
 source ./.github/workflows/ci/build.sh

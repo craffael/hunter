26188119417a179d0c520b3fccbfee8bdda09d08
craffael <raffael@casagrande.ch>
2021-03-16 22:09:29 +0100

    add package taskflow

---
+++ cmake/projects/taskflow/hunter.cmake
@@ -0,0 +1,34 @@
+# Copyright (c) 2016-2020, Rahul Sheth, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+include(hunter_cmake_args)
+include(hunter_configuration_types)
+
+hunter_add_version(
+    PACKAGE_NAME
+    taskflow
+    VERSION
+    3.0.0
+    URL
+    "https://github.com/taskflow/taskflow/archive/v3.0.0.tar.gz"
+    SHA1
+    2f14c813038c42c512756ac2287508a28c042304
+)
+
+hunter_cmake_args(
+    taskflow
+    CMAKE_ARGS
+        TF_BUILD_EXAMPLES=OFF
+        TF_BUILD_TESTS=OFF
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(taskflow)
+hunter_configuration_types(foo CONFIGURATION_TYPES Release) # Taskflow is header only
+hunter_download(PACKAGE_NAME taskflow)
---
+++ docs/packages/pkg/taskflow.rst
@@ -0,0 +1,20 @@
+.. spelling::
+
+    taskflow
+
+.. index::
+  single: concurrency ; taskflow
+
+.. _pkg.taskflow:
+
+taskflow
+========
+
+-  `Official <https://taskflow.github.io/>`__
+-  `Example <https://github.com/cpp-pm/hunter/blob/master/examples/taskflow/CMakeLists.txt>`__
+-  Added by `Raffael Casagrande <https://github.com/craffael>`__ (`pr-N <https://github.com/cpp-pm/hunter/pull/N>`__)
+
+.. literalinclude:: /../examples/taskflow/CMakeLists.txt
+  :language: cmake
+  :start-after: # DOCUMENTATION_START {
+  :end-before: # DOCUMENTATION_END }

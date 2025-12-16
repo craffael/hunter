9a271db3126f0a53ceea0cf617b0f0dd55419624
Raffael Casagrande <raffael@casagrande.ch>
2025-12-16 08:22:54 +0100

    patch alglib for newer cmake versions

--- cmake/projects/alglib/hunter.cmake
+++ cmake/projects/alglib/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     035b5076aad878c03dc9c2cdaf24d92c8dbb8c22
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    alglib
+    VERSION
+    3.18.0-p0
+    URL
+    "https://github.com/craffael/alglib/archive/v3.18.0-p1.tar.gz"
+    SHA1
+    e1ed6d7e0b46f2027e27da89407da26f1878db88
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(alglib)
 hunter_download(PACKAGE_NAME alglib)
